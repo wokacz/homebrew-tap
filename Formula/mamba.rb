bac138265c9a318bf6a395fe8e17b7cc492d660d
Łukasz Wokacz <lukasz@epicup.pl>
2025-03-24 14:02:47 +0100

    Update SHA256 checksum for Mamba formula

--- Formula/mamba.rb
+++ Formula/mamba.rb
@@ -2,7 +2,7 @@ class Mamba < Formula
   desc "CLI tool for initializing FastAPI projects"
   homepage "https://github.com/wokacz/mamba"
   url "https://github.com/wokacz/mamba/releases/download/0.1.1/mamba.tar.gz"
-  sha256 "04eac07b543956d1e03707f6baf6fada3cf3ddee1dc02fe12d7c4e476a855c7d"
+  sha256 "5a56fb9d8155aba7361bbf1ee6dffe14eff9cbb59c82e8e3e15980c56960cc17"
   license "MIT"
 
   def install
